e67bae021048af3a9ee125254cea64f122fa7841
Meg Dahlgren <meg.dahlgren@gmail.com>
2015-07-26 16:05:51 -0400

    changed passing parameters in all functions; program now gets zestimate, estimated mortgage payments, property taxes and hazard insurance

--- get_zestimate.py
+++ get_zestimate.py
@@ -17,7 +17,7 @@ my zws_id: X1-ZWz1a6057c0y6j_2rqd6
 
 
 
-def get_zpid(params_dict):
+def get_zpid(zwsid, address, citystatezip):
     '''
     :param: params_dict: dictionary with parameters for the GetSearchResults API call (zws-id, address, citystatezip)
     zwsid: Zillow web service id
@@ -26,16 +26,17 @@ def get_zpid(params_dict):
     :return: zpid for the property address
     '''
     #sample call = 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=<ZWSID>&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA'
-
+    params_dict = {'zws-id': zwsid, 'address': address, 'citystatezip': citystatezip}
     url = 'http://www.zillow.com/webservice/GetSearchResults.htm'
     data = requests.get(url, params=params_dict)
     #print data.url
+    #print data.text
     root = ET.fromstring(data.text)
     for zpid_el in root.iter('zpid'):
         return zpid_el.text
 
 
-def get_zestimate(params_dict):
+def get_zestimate(zwsid, zpid):
     '''
     :param: params_dict: dictionary with parameters for GetZestimate API call (zws-id, zpid)
     zws-id: Zillow web service id
@@ -43,16 +44,19 @@ def get_zestimate(params_dict):
     :return: dictionary containing price formatted as currency and as an ordinary string (e.g., '123456')
     '''
     price_dict = {}
+    #print zpid
+    params_dict = {'zws-id': zwsid, 'zpid': zpid}
     url = 'http://www.zillow.com/webservice/GetZestimate.htm'
     data = requests.get(url, params=params_dict)
+    #print data.url
     root = ET.fromstring(data.text)
     locale.setlocale(locale.LC_ALL, 'en_US')
     for amount_el in root.iter('amount'):
         price_dict['zestimate'] = amount_el.text
-        price_dict['zestimate_currency'] = locale.currency(int(amount_el.text), grouping=True)
+        price_dict['zestimate_currency'] = format_as_currency(amount_el.text)
         return price_dict
 
-def get_monthly_mortgage_pmt(params_dict):
+def get_monthly_mortgage_pmt(zwsid, price, down=20, dollarsdown=None, zip=None):
     '''
     :param: params_dict: dictionary containing parameters for GetMonthlyPayments API call url
     zws-id: Zillow web service id
@@ -65,33 +69,44 @@ def get_monthly_mortgage_pmt(params_dict):
     '''
     #sample call: http://www.zillow.com/webservice/GetMonthlyPayments.htm?zws-id=<ZWSID>&price=300000&down=15&zip=98104
     #data_dict_to_return = {} for now, returning json object and not using this
+    params_dict = {'zws-id': zwsid, 'price': price, 'zip': zip, 'output': 'json'}
+    if dollarsdown != None:
+        params_dict['dollarsdown'] = dollarsdown
+    else:
+        params_dict['down'] = down
     url = 'http://www.zillow.com/webservice/GetMonthlyPayments.htm'
     data = requests.get(url, params=params_dict)
     data = json.loads(data.text)
     #print json.dumps(data, sort_keys=True, indent=4)
     return data
 
-
+def format_as_currency(num_as_text):
+    return locale.currency(int(num_as_text), grouping=True)
 
 def main():
-    zwsid = 'X1-ZWz1a6057c0y6j_2rqd6' # since this isn't going to change, would this be an instance when a global variable would be ok?
-    url_params_dict = {'zws-id': zwsid, 'address': '93 Hillside Ave', 'citystatezip': 'Metuchen NJ 08840'}
-    test_zpid = get_zpid(url_params_dict)
-    url_params_dict['zpid'] = test_zpid
-    test_zestimate_dict = get_zestimate(url_params_dict)
+    zwsid = 'X1-ZWz1a6057c0y6j_2rqd6'
+    test_zpid = get_zpid(zwsid, '93 Hillside Ave', 'Metuchen NJ')
+    test_zestimate_dict = get_zestimate(zwsid, test_zpid)
     print 'Zestimate:', test_zestimate_dict['zestimate_currency']
-    url_params_dict['price'] = test_zestimate_dict['zestimate']
-    url_params_dict['zip'] = '08840'
-    url_params_dict['down']= '20'
-    monthly_housing_exp_data = get_monthly_mortgage_pmt(url_params_dict)
+    monthly_housing_exp_data = get_monthly_mortgage_pmt(zwsid, test_zestimate_dict['zestimate'], down=20, zip='08840')
     monthly_pmt_fifteen_fixed = monthly_housing_exp_data['response']['fifteenYearFixed']['monthlyPrincipalAndInterest']
     monthly_pmt_thirty_fixed = monthly_housing_exp_data['response']['thirtyYearFixed']['monthlyPrincipalAndInterest']
     down_pmt_fifteen_fixed = monthly_housing_exp_data['response']['downPayment']
     monthly_pmt_five_one_arm = monthly_housing_exp_data['response']['fiveOneARM']['monthlyPrincipalAndInterest']
-    #print 'Down payment amount (15-year fixed):', down_pmt_fifteen_fixed
-    #print 'Estimated mortgage payment (15-year fixed):', monthly_pmt_fifteen_fixed
-    #print 'Estimated mortgage payment (30-year fixed):', monthly_pmt_thirty_fixed
-    #print 'Estimated mortgage payment (five-one ARM):', monthly_pmt_five_one_arm
+    print 'Down payment amount (15-year fixed):', format_as_currency(down_pmt_fifteen_fixed)
+    print 'Estimated mortgage payment (15-year fixed):', format_as_currency(monthly_pmt_fifteen_fixed)
+    print 'Estimated mortgage payment (30-year fixed):', format_as_currency(monthly_pmt_thirty_fixed)
+    print 'Estimated mortgage payment (five-one ARM):', format_as_currency(monthly_pmt_five_one_arm)
+    if monthly_housing_exp_data['response']['monthlyHazardInsurance'] != None:
+        monthly_hazard_ins_pmt = monthly_housing_exp_data['response']['monthlyHazardInsurance']
+        print 'Estimated monthly hazard insurance premium:', format_as_currency(monthly_hazard_ins_pmt)
+    if monthly_housing_exp_data['response']['monthlyPropertyTaxes'] != None:
+        monthly_property_tax_pmt = monthly_housing_exp_data['response']['monthlyPropertyTaxes']
+        print 'Estimated property taxes:', format_as_currency(monthly_property_tax_pmt)
+
+
+
+
 
 
 
